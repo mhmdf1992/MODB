abeaed6335631672da61a9709b38cd0773aeaa3f
mhmd <mhmdfayad1992@gmail.com>
2023-07-28 17:32:42 +0300

    add json property attribute

--- Client/MODBError.cs
+++ Client/MODBError.cs
@@ -1,19 +1,30 @@
 using System.Collections.Generic;
-
+using System.Text.Json.Serialization;
 namespace MODB.Client{
     public class MODBError{
+        [JsonPropertyName("statusCode")]
         public int StatusCode {get; set;}
+        [JsonPropertyName("statusMessage")]
         public string StatusMessage {get; set;}
+        [JsonPropertyName("traceId")]
+        public string TraceId {get; set;}
+        [JsonPropertyName("errorMessage")]
         public string ErrorMessage {get; set;}
+        [JsonPropertyName("errors")]
         public IEnumerable<Error> Errors {get; set;}
     }
 
     public class Error
     {
+        [JsonPropertyName("code")]
         public object Code { get; set; }
+        [JsonPropertyName("field")]
         public string Field { get; set; }
+        [JsonPropertyName("attemptedValue")]
         public object AttemptedValue { get; set; }
+        [JsonPropertyName("message")]
         public string Message { get; set; }
+        [JsonPropertyName("helpURL")]
         public string HelpURL { get; set; }
     }
 }
