62919ebd8b71094f8211cf42f319cc97eb4e18cc
mhmd <mhmdfayad1992@gmail.com>
2023-07-28 17:31:16 +0300

    add @ to allowed chars key

--- FlatFileDB/Validator.cs
+++ FlatFileDB/Validator.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace MODB.FlatFileDB{
     public class Validator{
-        static readonly char[] KEY_ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_.-".ToArray();
-        public static bool ValidateKey(string key) => string.IsNullOrEmpty(key) || key.Any(x => !KEY_ALLOWED_CHARS.Contains(x)) ? throw new ArgumentException($"{key} is not a valid key. keys must match ^[a-zA-Z0-9_.-]+$", nameof(key)) : true;
+        static readonly char[] KEY_ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_.-@".ToArray();
+        public static bool ValidateKey(string key) => string.IsNullOrEmpty(key) || key.Any(x => !KEY_ALLOWED_CHARS.Contains(x)) ? throw new ArgumentException($"{key} is not a valid key. keys must match ^[a-zA-Z0-9_.-@]+$", nameof(key)) : true;
     }
 }
